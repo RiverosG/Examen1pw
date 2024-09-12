94150beae8413ca5e6ba866dae1fb5b1b298550d
O23438 <O23438@lab.aca>
2024-09-11 21:43:51 -0500

    ejercicios1y2
    Desarrollo de los ejercicios 1, 2 y subida a git

---
+++ Pregunta2/pregunta2.js
@@ -0,0 +1,11 @@
+document.getElementById("Altura").innerHTML = Math.random()
+let altura = document.getElementById("Altura")
+document.getElementById("Peso").innerHTML = Math.random()
+let peso = document.getElementById("Peso")
+
+console.log(altura*peso)
+let IMC = altura*peso
+document.getElementById("imc").innerHTML = IMC
+
+
+
